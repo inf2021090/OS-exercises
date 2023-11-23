aacc50c5022bc5c3470840be0d807f585b192141
inf2021090 <inf2021090@ionio.gr>
2023-11-23 16:47:55 +0000

    updated mycopy

--- mycopy.c
+++ mycopy.c
@@ -8,18 +8,18 @@
 
 #define DEFAULT_BUFFER_SIZE 1024
 
-ssize_t copyFile(const char *source, const char *destination, size_t buffer_size) {
+ssize_t myCopy(const char *src, const char *dst, size_t buffer_size) {
     int source_fd, dest_fd;
     ssize_t bytes_written = 0;
     char buffer[buffer_size];
 
-    source_fd = open(source, O_RDONLY);
+    source_fd = open(src, O_RDONLY);
     if (source_fd == -1) {
         perror("Source file cannot be opened for reading!");
         return -1;
     }
 
-    if ((dest_fd = open(destination, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1) {
+    if ((dest_fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1) {
         perror("Destination file cannot be opened for writing!");
         close(source_fd);
         return -1;
@@ -44,22 +44,22 @@ ssize_t copyFile(const char *source, const char *destination, size_t buffer_size
 
 int main(int argc, char **argv) {
     size_t buffer_size = DEFAULT_BUFFER_SIZE;
-    const char *source;
-    const char *destination;
+    const char *src;
+    const char *dst;
 
     if (argc == 3) {
-        source = argv[1];
-        destination = argv[2];
+        src = argv[1];
+        dst = argv[2];
     } else if (argc == 5 && strcmp(argv[1], "-b") == 0) {
         buffer_size = atoi(argv[2]);
-        source = argv[3];
-        destination = argv[4];
+        src = argv[3];
+        dst = argv[4];
     } else {
         printf("Usage: mycopy [-b buffer_size] source_file destination_file\n");
         exit(1);
     }
 
-    ssize_t bytes_copied = copyFile(source, destination, buffer_size);
+    ssize_t bytes_copied = myCopy(src, dst, buffer_size);
     if (bytes_copied != -1) {
         printf("File copied successfully. Bytes copied: %zd\n", bytes_copied);
     } else {
