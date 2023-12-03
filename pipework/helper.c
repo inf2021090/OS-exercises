145768b33f8b4b9ebaf82fbeba1e2842411c0297
inf2021090 <inf2021090@ionio.gr>
2023-12-03 16:28:52 +0000

    removed extra output line

--- pipework/helper.c
+++ pipework/helper.c
@@ -10,18 +10,21 @@ int main(int argc, char* argv[]){
 
   if (argc != 2){
     fprintf(stderr, "Usage: %s <infile>\n", argv[0]);
-    exit(303);
+    printf("Error code: %d\n", 3003);
+    exit(EXIT_FAILURE);
   }
 
   int fd[2];
   if (pipe(fd) == -1){
     perror("Pipe failed!");
-    exit(10);
+    printf("Error code: %d\n", 10);
+    exit(0);
   }
 
   int pid = fork();
   if (pid == -1){
     perror("Fork failed!");
+    printf("Error code: %d\n", errno);
     exit(20);
   }
 
@@ -34,19 +37,25 @@ int main(int argc, char* argv[]){
 
   } else { //parent process
 		close(fd[1]); // close writing end of pipe
-		
+
 		char ch;
-		printf("Data received through pipe: ");
+		int newLine = 1; // Flag to indicate start of a new line
 		
 		// read from the pipe and print received data line by line
 		while (read(fd[0], &ch, 1) > 0) {
+		    if (newLine) {
+		        printf("Data received through pipe: ");
+		        newLine = 0;
+		    }
+		
 		    if (ch == '\n') {
-		        printf("\nData received through pipe: ");
+		        putchar('\n');
+		        newLine = 1;
 		    } else {
 		        putchar(ch);
 		    }
-		}
-		
+}
+
 		printf("\n"); // ensure a newline after the last line
 		wait(NULL); // wait for child process to finish
     close(fd[0]); // close reading end of pipe
